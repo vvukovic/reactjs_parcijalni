34e293dd0bf444c9290136f35079388f597c666e
Vedran Vukovic <vedran@outlook>
2020-07-13 18:55:23 +0200

    Adding event onChange to save values from user input into state

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 export default class SearchForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            searchQuery: '',
+            githubUserData: {}
+        }
+    }
+
+    handleQueryChange = (event) => {
+        this.setState({ searchQuery: event.target.value });
     }
 
     render() {
@@ -10,7 +19,7 @@ export default class SearchForm extends React.Component {
             <div>
                 <h5>Github korisničko ime:</h5>
                 <form>
-                    <input type="text" value="" placeholder="npr. reactjs" />
+                    <input type="text" value={this.state.searchQuery} onChange={this.handleQueryChange} placeholder="npr. reactjs" />
                     <input type="submit" value="Pretraži Github" />
                 </form>
             </div>   
